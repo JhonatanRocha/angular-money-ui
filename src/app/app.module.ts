22e80e2ebd626cd53138d94a3fa905aaea176aae
JhonatanRocha <jcristianrocha@gmail.com>
2018-10-15 17:35:20 -0300

    Criacao do modulo de pessoa e lancamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,57 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputMaskModule } from 'primeng/inputmask';
-import { KeyFilterModule } from 'primeng/keyfilter';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from 'primeng/calendar';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { ButtonModule } from 'primeng/button';
-import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { DropdownModule } from 'primeng/dropdown';
-import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-import { FormsModule } from '@angular/forms';
-import { InteraMessageComponent } from './intera-message/intera-message.component';
-import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
+import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { InteraMessageComponent } from './intera-message/intera-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent,
-    LancamentoCadastroComponent,
-    PessoaCadastroComponent,
     InteraMessageComponent,
-    LancamentosGridComponent,
-    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    InputTextModule,
-    ButtonModule,
-    DataTableModule,
-    TooltipModule,
-    InputTextareaModule,
     BrowserAnimationsModule,
-    CalendarModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputMaskModule,
-    KeyFilterModule,
-    CurrencyMaskModule
+    LancamentosModule,
+    PessoasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { KeyFilterModule } from 'primeng/keyfilter';
+
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputMaskModule,
+    KeyFilterModule
+  ],
+  declarations: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent,
+    PessoasGridComponent
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule { }
